b35786ba088a24c6105b2fbcb25e58c090f159d8
Anshul Soni <64320376+AnshulAmrev@users.noreply.github.com>
2021-06-04 19:50:16 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+ <footer class="foo">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;">NOTES MMDU <br><br><br>MADE BY:<br>ANSHUL, ARJUN, JATIN & KARAN</p>
+                </div>
+            </div>
+   </footer>
